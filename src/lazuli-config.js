ac9d6880dc343126b03bdd7182748a367c584597
Nico Hauser <me@tyratox.ch>
2017-08-27 12:46:24 +0200

    Updated to be synchronous

--- src/lazuli-config.js
+++ src/lazuli-config.js
@@ -1,83 +1,66 @@
-const logger = require("lazuli-logger");
-
-const util = require("util");
 const fs = require("fs");
 const path = require("path");
 
-const stat = util.promisify(fs.stat);
-const readdir = util.promisify(fs.readdir);
-const writeFile = util.promisify(fs.writeFile);
+const statSync = fs.statSync;
+const readdirSync = fs.readdirSync;
+const writeFileSync = fs.writeFileSync;
+const existsSync = fs.existsSync;
 
 //First of all we have to find the applications root directory
 let root = __dirname.split("node_modules")[0];
-logger.log("info", "Found root directory: ", root);
+console.log("info", "Found root directory: ", root);
 
-let config;
+/**
+ * Scans a directory for lazuli modules and adds their default values from
+ * config-sample to the config object if the key isn't present yet
+ * @param  {Object} config The config object
+ * @param  {String} folder The folder to scan
+ * @return {Promise}       A promise to check for the success
+ */
+const scanDirectorySync = (config, folder) => {
+	if (!existsSync(path.join(folder, "node_modules"))) {
+		return;
+	}
 
-stat(path.join(root, "config.json"))
-	.then(stats => {
-		if (stats.isFile()) {
-			config = require(path.join(root, "config.json"));
-		} else {
-			config = require(path.join(root, "config-sample.json"));
-		}
+	console.log("info", "scanning", path.join(folder, "node_modules"), "...");
 
-		logger.log("info", "Loaded root config");
+	const files = readdirSync(path.join(folder, "node_modules"));
+	let defaultOptions = {};
 
-		logger.log(
-			"info",
-			"Searching ",
-			path.join(root, "node_modules"),
-			" for lazuli modules.."
-		);
-
-		return readdir(path.join(root, "node_modules"));
-	})
-	.then(files => {
-		return Promise.all(
-			files.map(file => {
-				if (file.startsWith("lazuli-")) {
-					return stat(path.join(root, file)).then(() => {
-						stats => {
-							if (stats.isDirectory()) {
-								//valid lazuli module
-								return Promise.resolve(file);
-							}
-						};
-					});
-				} else {
-					return Promise.resolve(null);
+	files
+		.filter(file => {
+			if (file.startsWith("lazuli-")) {
+				if (
+					existsSync(path.join(folder, file)) &&
+					statSync(path.join(folder, file)).isDirectory()
+				) {
+					//valid lazuli module
+					return true;
 				}
-			})
-		);
-	})
-	.then(moduleArray => {
-		return Promise.all(
-			moduleArray.filter(module => module).map(module => {
-				return stat(path.join(root, file, "config-sample.json")).then(stats => {
-					if (stats.isFile()) {
-						logger.log("info", "Found config-sample.json for ", file);
-						return Promise.resolve(
-							require(path.join(root, file, "config-sample.json"))
-						);
-					} else {
-						return Promise.resolve(null);
-					}
-				});
-			})
-		);
-	})
-	.then(optionsArray => {
-		let defaultOptions = {};
+			}
 
-		optionsArray.filter(options => options).forEach(options => {
+			return false;
+		})
+		.map(module => {
+			if (
+				existsSync(path.join(folder, file, "config-sample.json")) &&
+				statSync(path.join(folder, file, "config-sample.json")).isFile()
+			) {
+				console.log("info", "Found config-sample.json for ", file);
+				return require(path.join(folder, file, "config-sample.json"));
+			} else {
+				return null;
+			}
+		})
+		.filter(options => options)
+		.forEach(options => {
 			for (let key in options) {
 				if (!options.hasOwnProperty(key)) {
 					continue;
 				}
 
 				if (key in defaultOptions) {
-					logger.log(
+					console.log(
 						"warning",
 						"Option key " +
 							key +
@@ -96,39 +79,53 @@ stat(path.join(root, "config.json"))
 			}
 		});
 
-		for (let key in defaultOptions) {
-			if (!defaultOptions.hasOwnProperty(key)) {
-				continue;
-			}
-
-			if (!key in config) {
-				logger.log(
-					"info",
-					"Option key " +
-						key +
-						" wasn't defined in the global config. Using default option " +
-						defaultOptions[key]
-				);
-				config[key] = defaultOptions[key];
-			}
+	for (let key in defaultOptions) {
+		if (!defaultOptions.hasOwnProperty(key)) {
+			continue;
 		}
 
-		return writeFile(
-			path.join(root, "config.json"),
-			JSON.stringify(config),
-			"utf8"
-		);
-	})
-	.then(() => {
-		logger.log(
-			"info",
-			"Successfully loaded all lazuli options and wrote configuration to " +
-				path.join(root, "config.json")
-		);
-	})
-	.catch(err => {
-		throw err;
-		process.exit(1);
-	});
+		if (!key in config) {
+			console.log(
+				"info",
+				"Option key " +
+					key +
+					" wasn't defined in the global config. Using default option " +
+					defaultOptions[key]
+			);
+			config[key] = defaultOptions[key];
+		}
+	}
 
+	return config;
+};
+
+let config;
+
+if (existsSync(path.join(root, "config.json"))) {
+	console.log("info", "importing config.json ...");
+	config = require(path.join(root, "config.json"));
+} else if (existsSync(path.join(root, "config-sample.json"))) {
+	console.log("info", "importing config-sample.json ...");
+	config = require(path.join(root, "config-sample.json"));
+} else {
+	console.log("info", "no config found!");
+	config = {};
+}
+
+scanDirectorySync(config, root);
+scanDirectorySync(config, path.resolve(__dirname, ".."));
+
+writeFileSync(path.join(root, "config.json"), JSON.stringify(config), "utf8");
+
+console.log(
+	"info",
+	"Successfully loaded",
+	Object.keys(config).length,
+	"lazuli options and wrote configuration to " + path.join(root, "config.json")
+);
+
+/**
+ * The combined config object
+ * @type {Object}
+ */
 module.exports = config;
--- test/test.js
+++ test/test.js
@@ -1,2 +1,16 @@
 import test from "ava";
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
+const unlink = util.promisify(fs.unlink);
+
 const config = require("../src/lazuli-config.js");
+
+test(t => {
+	t.deepEqual(config, {});
+});
+
+test.after.always("remove generated config.json", t => {
+	return unlink(path.resolve(__dirname, "..", "src", "config.json"));
+});
